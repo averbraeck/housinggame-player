902d76ca35f5ab575dceb8c58fc691cd4d8e6126
Alexander Verbraeck <a.verbraeck@gmail.com>
2023-11-23 18:37:44 +0100

    Remove unused imports.

--- src/main/java/nl/tudelft/simulation/housinggame/player/CheckHousesServlet.java
+++ src/main/java/nl/tudelft/simulation/housinggame/player/CheckHousesServlet.java
@@ -1,8 +1,6 @@
 package nl.tudelft.simulation.housinggame.player;
 
 import java.io.IOException;
-import java.util.SortedMap;
-import java.util.TreeMap;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -10,15 +8,6 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
-
-import org.jooq.DSLContext;
-import org.jooq.Result;
-import org.jooq.SQLDialect;
-import org.jooq.impl.DSL;
-import org.jooq.types.UInteger;
-
-import nl.tudelft.simulation.housinggame.data.Tables;
-import nl.tudelft.simulation.housinggame.data.tables.records.HouseRecord;
 
 @WebServlet("/check-houses")
 public class CheckHousesServlet extends HttpServlet
