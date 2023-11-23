18327f602363d46edb875f621db2f87c93043109
Alexander Verbraeck <a.verbraeck@gmail.com>
2023-11-23 06:54:35 +0100

    Full player state machine

--- src/main/java/nl/tudelft/simulation/housinggame/player/AdvanceStateServlet.java
+++ src/main/java/nl/tudelft/simulation/housinggame/player/AdvanceStateServlet.java
@@ -11,6 +11,8 @@ import javax.servlet.http.HttpSession;
 
 import nl.tudelft.simulation.housinggame.common.PlayerState;
 import nl.tudelft.simulation.housinggame.common.RoundState;
+import nl.tudelft.simulation.housinggame.data.tables.records.GrouproundRecord;
+import nl.tudelft.simulation.housinggame.data.tables.records.PlayerroundRecord;
 
 /**
  * AdvanceStateServlet is called when the OK button at the bottom of the player scren is pressed, and the player wants to
@@ -42,31 +44,122 @@ public class AdvanceStateServlet extends HttpServlet
 
         // reload with the latest state
         data.readDynamicData();
+        PlayerState playerState = PlayerState.valueOf(data.getPlayerRound().getPlayerState());
+        RoundState roundState = RoundState.valueOf(data.getGroupRound().getRoundState());
 
         String okButton = "";
         if (request.getParameter("okButton") != null)
             okButton = request.getParameter("okButton");
 
-        // are we in the same round as the group?
-        if (data.getPlayerRoundNumber() != data.getGroupRoundNumber())
+        if (okButton.equals("welcome-wait"))
         {
-            data.setError("data.getPlayerRoundNumber(" + data.getPlayerRoundNumber() + ") != data.getGroupRoundNumber("
-                    + data.getGroupRoundNumber() + ")");
-            response.sendRedirect("/housinggame-player/error");
+            if (!playerState.equals(PlayerState.LOGIN))
+                System.err.println("playerFinish = 'welcome-wait', but player state is '" + playerState + "'");
+            if (roundState.nr < RoundState.NEW_ROUND.nr)
+                System.err.println("playerFinish = 'welcome-wait', but group state is '" + roundState + "'");
+            if (data.getPlayerRoundNumber() != 0)
+            {
+                data.setError("jsp = 'welcome-wait', but player round is " + data.getPlayerRoundNumber()
+                + ", and player state is '" + playerState + "'");
+                response.sendRedirect("/housinggame-player/error");
+            }
+            if (data.getGroupRoundNumber() == 0)
+            {
+                data.setError("jsp = 'welcome-wait', but group round is " + data.getGroupRoundNumber());
+                response.sendRedirect("/housinggame-player/error");
+            }
+            // advance to round 1
+            GrouproundRecord groupRound1 = data.getGroupRoundList().get(1);
+            PlayerroundRecord prr = SqlUtils.makePlayerRound(data, groupRound1);
+            prr.setPlayerState(PlayerState.READ_BUDGET.toString());
+            prr.store();
+            data.readDynamicData();
+            response.sendRedirect("/housinggame-player/read-budget");
             return;
         }
 
-        if (okButton.equals("welcome-wait"))
+        if (okButton.equals("read-budget"))
         {
-            if (!data.getPlayerRound().getPlayerState().equals(PlayerState.LOGIN.toString()))
-                System.err.println(
-                        "playerFinish = 'welcome-wait', but player state is '" + data.getPlayerRound().getPlayerState() + "'");
-            if (RoundState.lt(data.getGroupRound().getRoundState(), RoundState.NEW_ROUND.toString()))
-                System.err.println(
-                        "playerFinish = 'welcome-wait', but group state is '" + data.getGroupRound().getRoundState() + "'");
-            data.getPlayerRound().setPlayerState(PlayerState.READ_BUDGET.toString());
+            data.getPlayerRound().setPlayerState(PlayerState.READ_NEWS.toString());
             data.getPlayerRound().store();
+            response.sendRedirect("/housinggame-player/read-news");
+            return;
+        }
+
+        if (okButton.equals("read-news"))
+        {
+            data.getPlayerRound().setPlayerState(PlayerState.CHECK_HOUSES.toString());
+            data.getPlayerRound().store();
+            response.sendRedirect("/housinggame-player/check-houses");
+            return;
+        }
+
+        if (okButton.equals("check-houses"))
+        {
+            data.getPlayerRound().setPlayerState(PlayerState.BOUGHT_HOUSE.toString());
+            data.getPlayerRound().store();
+            response.sendRedirect("/housinggame-player/bought-house");
+            return;
+        }
+
+        if (okButton.equals("bought-house"))
+        {
+            data.getPlayerRound().setPlayerState(PlayerState.VIEW_TAXES.toString());
+            data.getPlayerRound().store();
+            response.sendRedirect("/housinggame-player/view-taxes");
+            return;
+        }
+
+        if (okButton.equals("view-taxes"))
+        {
+            data.getPlayerRound().setPlayerState(PlayerState.IMPROVEMENTS.toString());
+            data.getPlayerRound().store();
+            response.sendRedirect("/housinggame-player/improvements");
+            return;
+        }
+
+        if (okButton.equals("improvements"))
+        {
+            // TODO handle the entered improvements, create measure records
+            data.getPlayerRound().setPlayerState(PlayerState.ANSWER_SURVEY.toString());
+            data.getPlayerRound().store();
+            response.sendRedirect("/housinggame-player/answer-survey");
+            return;
+        }
+
+        if (okButton.equals("improvements"))
+        {
+            // TODO handle the entered survey answers, create answer records
+            data.getPlayerRound().setPlayerState(PlayerState.VIEW_DAMAGE.toString());
+            data.getPlayerRound().store();
+            response.sendRedirect("/housinggame-player/view-damage");
+            return;
+        }
+
+        if (okButton.equals("view-damage"))
+        {
+            data.getPlayerRound().setPlayerState(PlayerState.SUMMARY.toString());
+            data.getPlayerRound().store();
+            response.sendRedirect("/housinggame-player/summary");
+            return;
+        }
+
+        if (okButton.equals("summary"))
+        {
+            // advance to next round
+            int roundNr = data.getPlayerRoundNumber();
+            if (data.getGroupRoundList().size() <= roundNr + 1)
+            {
+                data.setError("jsp = 'summary' -> 'new-round', but group round is too low: " + data.getGroupRoundNumber());
+                response.sendRedirect("/housinggame-player/error");
+            }
+            GrouproundRecord groupRoundN = data.getGroupRoundList().get(roundNr + 1);
+            PlayerroundRecord prr = SqlUtils.makePlayerRound(data, groupRoundN);
+            prr.setPlayerState(PlayerState.READ_BUDGET.toString());
+            prr.store();
+            data.readDynamicData();
             response.sendRedirect("/housinggame-player/read-budget");
+            return;
         }
 
         response.sendRedirect("/housinggame-player/login");
