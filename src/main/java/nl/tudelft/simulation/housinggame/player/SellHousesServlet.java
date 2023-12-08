d70c55e2ca879b60bce82d96116a025689c1f171
Alexander Verbraeck <a.verbraeck@gmail.com>
2023-12-08 14:31:47 +0100

    Rename screens to reflect new game flow

---
+++ src/main/java/nl/tudelft/simulation/housinggame/player/SellHousesServlet.java
@@ -0,0 +1,51 @@
+package nl.tudelft.simulation.housinggame.player;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+@WebServlet("/sell-house")
+public class SellHousesServlet extends HttpServlet
+{
+
+    /** */
+    private static final long serialVersionUID = 1L;
+
+    @Override
+    protected void doPost(final HttpServletRequest request, final HttpServletResponse response)
+            throws ServletException, IOException
+    {
+        HttpSession session = request.getSession();
+
+        PlayerData data = SessionUtils.getData(session);
+        if (data == null)
+        {
+            response.sendRedirect("/housinggame-player/login");
+            return;
+        }
+
+        data.getContentHtml().clear();
+        ContentUtils.makeBudgetAccordion(data);
+        ContentUtils.makeNewsAccordion(data);
+
+        if (ContentUtils.makeHousesAccordion(data))
+            response.sendRedirect("jsp/player/sell-house.jsp");
+        else
+        {
+            // TODO: make error screen instead
+            response.sendRedirect("jsp/player/login.jsp");
+        }
+    }
+
+    @Override
+    protected void doGet(final HttpServletRequest req, final HttpServletResponse resp) throws ServletException, IOException
+    {
+        doPost(req, resp);
+    }
+
+}
