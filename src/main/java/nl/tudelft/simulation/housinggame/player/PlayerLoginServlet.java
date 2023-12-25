390b84599f6a6a6716c5283c17f48f1e9b3658a0
Alexander Verbraeck <a.verbraeck@gmail.com>
2023-12-25 14:26:39 +0100

    Moved initial redirect function to login servlet
    (only used at login)

--- src/main/java/nl/tudelft/simulation/housinggame/player/PlayerLoginServlet.java
+++ src/main/java/nl/tudelft/simulation/housinggame/player/PlayerLoginServlet.java
@@ -13,6 +13,8 @@ import org.jooq.DSLContext;
 import org.jooq.SQLDialect;
 import org.jooq.impl.DSL;
 
+import nl.tudelft.simulation.housinggame.common.PlayerState;
+import nl.tudelft.simulation.housinggame.common.RoundState;
 import nl.tudelft.simulation.housinggame.data.Tables;
 import nl.tudelft.simulation.housinggame.data.tables.records.GamesessionRecord;
 import nl.tudelft.simulation.housinggame.data.tables.records.GroupRecord;
@@ -113,7 +115,7 @@ public class PlayerLoginServlet extends HttpServlet
 
         if (ok)
         {
-            PlayerStateUtils.redirect(data, response);
+            redirect(data, response);
         }
         else
         {
@@ -135,4 +137,103 @@ public class PlayerLoginServlet extends HttpServlet
     {
         response.sendRedirect("jsp/player/login.jsp");
     }
+
+    public static void redirect(final PlayerData data, final HttpServletResponse response) throws IOException, ServletException
+    {
+        data.setError("");
+        data.readDynamicData();
+        PlayerState playerState = PlayerState.valueOf(data.getPlayerRound().getPlayerState());
+        RoundState roundState = RoundState.valueOf(data.getGroupRound().getRoundState());
+        if (playerState == null)
+        {
+            data.setError("Player " + data.getPlayerCode() + " is in illegal state: " + data.getPlayerRound().getPlayerState()
+                    + "<br>Log out and log in again when problem has been corrected.");
+            response.sendRedirect("/housinggame-player/error");
+            return;
+        }
+        if (data.getPlayerRoundNumber() > data.getGroupRoundNumber())
+        {
+            data.setError("Player " + data.getPlayerCode() + " is in Round " + data.getPlayerRoundNumber()
+                    + ", but group is only in Round " + data.getGroupRoundNumber()
+                    + "<br>Log out and log in again when group has advanced to the same round.");
+            response.sendRedirect("/housinggame-player/error");
+            return;
+        }
+        if (data.getPlayerRoundNumber() == data.getGroupRoundNumber() && playerState.nr > roundState.nr)
+        {
+            data.setError("Player " + data.getPlayerCode() + " is in State " + playerState + ", but group is only in State "
+                    + roundState + "<br>Log out and log in again when group has advanced to the same state.");
+            response.sendRedirect("/housinggame-player/error");
+            return;
+        }
+
+        switch (playerState)
+        {
+            case LOGIN:
+                response.sendRedirect("/housinggame-player/welcome-wait");
+                break;
+
+            case READ_BUDGET:
+                response.sendRedirect("/housinggame-player/read-budget");
+                break;
+
+            case READ_NEWS:
+                response.sendRedirect("/housinggame-player/read-news");
+                break;
+
+            case VIEW_SELL_HOUSE:
+                response.sendRedirect("/housinggame-player/sell-house");
+                break;
+
+            case SELL_HOUSE_WAIT:
+                response.sendRedirect("/housinggame-player/sell-house-wait");
+                break;
+
+            case STAY_HOUSE_WAIT:
+                response.sendRedirect("/housinggame-player/stay-house-wait");
+                break;
+
+            case VIEW_BUY_HOUSE:
+                response.sendRedirect("/housinggame-player/buy-house");
+                break;
+
+            case BUY_HOUSE_WAIT:
+                response.sendRedirect("/housinggame-player/buy-house-wait");
+                break;
+
+            case BOUGHT_HOUSE:
+                response.sendRedirect("/housinggame-player/bought-house");
+                break;
+
+            case STAYED_HOUSE:
+                response.sendRedirect("/housinggame-player/sell-house-stay");
+                break;
+
+            case VIEW_IMPROVEMENTS:
+                response.sendRedirect("/housinggame-player/view-improvements");
+                break;
+
+            case ANSWER_SURVEY:
+                response.sendRedirect("/housinggame-player/answer-survey");
+                break;
+
+            case SURVEY_COMPLETED:
+                response.sendRedirect("/housinggame-player/survey-completed");
+                break;
+
+            case VIEW_DAMAGE:
+                response.sendRedirect("/housinggame-player/view-damage");
+                break;
+
+            case VIEW_SUMMARY:
+                response.sendRedirect("/housinggame-player/view-summary");
+                break;
+
+            default:
+                throw new IllegalArgumentException(
+                        "Unexpected value for PlayerState: " + data.getPlayerRound().getPlayerState());
+        }
+
+    }
+
 }
