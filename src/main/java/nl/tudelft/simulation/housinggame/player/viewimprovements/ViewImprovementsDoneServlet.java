ee5196b23307877f9d7cf2f71df09033214bd5b2
Alexander Verbraeck <a.verbraeck@gmail.com>
2023-12-31 02:11:32 +0100

    Issue #10. Process selected improvements and store in database

--- src/main/java/nl/tudelft/simulation/housinggame/player/viewimprovements/ViewImprovementsDoneServlet.java
+++ src/main/java/nl/tudelft/simulation/housinggame/player/viewimprovements/ViewImprovementsDoneServlet.java
@@ -45,77 +45,101 @@ public class ViewImprovementsDoneServlet extends HttpServlet
             return;
         }
 
-        // the next screen button indicates the INTENTION of the player, not the screen it originates from.
-        String nextScreen = request.getParameter("nextScreen");
-
-        // player clicked MOVE IN on the buy-house-wait screen
-        if (nextScreen.equals("answer-survey"))
+        String satPointsStr = request.getParameter("form-selected-points");
+        if (satPointsStr == null)
         {
-            // reload the round with the latest state
-            data.readDynamicData();
-            PlayerroundRecord prr = data.getPlayerRound();
-            HousegroupRecord hgr = SqlUtils.readRecordFromId(data, Tables.HOUSEGROUP, prr.getFinalHousegroupId());
-
-            // return OK if the button for the current screen can be enabled, an empty string otherwise
-            DSLContext dslContext = DSL.using(data.getDataSource(), SQLDialect.MYSQL);
-            WelfaretypeRecord wft = SqlUtils.readRecordFromId(data, Tables.WELFARETYPE, data.getPlayer().getWelfaretypeId());
-            List<MeasureRecord> measureList = dslContext.selectFrom(Tables.MEASURE)
-                    .where(Tables.MEASURE.HOUSEGROUP_ID.eq(prr.getFinalHousegroupId())).fetch();
-            int measureCost = 0;
-            int measureSat = 0;
-            String formOptions = request.getParameter("form-options").strip();
-            // regex escape characters: <([{\^-=$!|]})?*+.>
-            if (formOptions.length() > 1)
-            {
-                String[] measureArray = formOptions.split("&");
-                for (String m : measureArray)
-                {
-                    String measureTypeIdStr = m.split("\\=")[1].strip();
-                    int measureTypeId = Integer.parseInt(measureTypeIdStr);
-                    MeasuretypeRecord mt = SqlUtils.readRecordFromId(data, Tables.MEASURETYPE, measureTypeId);
-                    boolean found = false;
-                    for (MeasureRecord mr : measureList)
-                    {
-                        if (mr.getMeasuretypeId().equals(measureTypeId))
-                            found = true;
-                    }
-                    if (!found) // new measure
-                    {
-                        MeasureRecord measure = dslContext.newRecord(Tables.MEASURE);
-                        measure.setRoundNumber(data.getGroupRound().getRoundNumber());
-                        measure.setMeasuretypeId(mt.getId());
-                        measure.setHousegroupId(hgr.getId());
-                        measure.setConsumedInRound(null);
-                        measure.store();
-                        measureCost += mt.getPrice();
-                        measureSat += mt.getSatisfactionDelta();
-                    }
-                }
-            }
-
-            String satPointsStr = request.getParameter("form-selected-points").strip();
-            int satPoints = Integer.parseInt(satPointsStr);
-            int satPointCost = satPoints * wft.getSatisfactionCostPerPoint();
-
-            hgr.setHouseSatisfaction(hgr.getHouseSatisfaction() + measureSat);
-            prr.setSatisfactionHouseMeasures(measureSat);
-            prr.setCostMeasuresBought(measureCost);
-            hgr.store();
-
-            prr.setSatisfactionBought(satPoints);
-            prr.setCostSatisfactionBought(satPointCost);
-            prr.setPersonalSatisfaction(prr.getPersonalSatisfaction() + satPoints);
-            prr.setSpendableIncome(prr.getSpendableIncome() - measureCost - satPointCost);
-            prr.setPlayerState(PlayerState.ANSWER_SURVEY.toString());
-            prr.store();
-
-            response.sendRedirect("/housinggame-player/answer-survey");
+            data.errorRedirect(response,
+                    "Player app called view-improvements-done servlet, but form-selected-points field is null");
             return;
         }
+        satPointsStr = satPointsStr.strip();
 
-        // if the player did not click 'view house' and enters the read-news-done servlet, something is wrong
-        System.err.println("Player app called view-improvements-done servlet, but NextScreen button is " + nextScreen);
-        response.sendRedirect("/housinggame-player/login");
+        String formOptions = request.getParameter("form-options");
+        if (formOptions == null)
+        {
+            data.errorRedirect(response, "Player app called view-improvements-done servlet, but form-options field is null");
+            return;
+        }
+        formOptions = formOptions.strip();
+
+        try
+        {
+            // the next screen button indicates the INTENTION of the player, not the screen it originates from.
+            String nextScreen = request.getParameter("nextScreen");
+
+            // player clicked MOVE IN on the buy-house-wait screen
+            if (nextScreen.equals("answer-survey"))
+            {
+                // reload the round with the latest state
+                data.readDynamicData();
+                PlayerroundRecord prr = data.getPlayerRound();
+                HousegroupRecord hgr = SqlUtils.readRecordFromId(data, Tables.HOUSEGROUP, prr.getFinalHousegroupId());
+
+                // return OK if the button for the current screen can be enabled, an empty string otherwise
+                DSLContext dslContext = DSL.using(data.getDataSource(), SQLDialect.MYSQL);
+                WelfaretypeRecord wft =
+                        SqlUtils.readRecordFromId(data, Tables.WELFARETYPE, data.getPlayer().getWelfaretypeId());
+                List<MeasureRecord> measureList = dslContext.selectFrom(Tables.MEASURE)
+                        .where(Tables.MEASURE.HOUSEGROUP_ID.eq(prr.getFinalHousegroupId())).fetch();
+                int measureCost = 0;
+                int measureSat = 0;
+                if (formOptions.length() > 1)
+                {
+                    // regex escape characters: <([{\^-=$!|]})?*+.>
+                    String[] measureArray = formOptions.split("&");
+                    for (String m : measureArray)
+                    {
+                        String measureTypeIdStr = m.split("\\=")[1].strip();
+                        int measureTypeId = Integer.parseInt(measureTypeIdStr);
+                        MeasuretypeRecord mt = SqlUtils.readRecordFromId(data, Tables.MEASURETYPE, measureTypeId);
+                        boolean found = false;
+                        for (MeasureRecord mr : measureList)
+                        {
+                            if (mr.getMeasuretypeId().equals(measureTypeId))
+                                found = true;
+                        }
+                        if (!found) // new measure
+                        {
+                            MeasureRecord measure = dslContext.newRecord(Tables.MEASURE);
+                            measure.setRoundNumber(data.getGroupRound().getRoundNumber());
+                            measure.setMeasuretypeId(mt.getId());
+                            measure.setHousegroupId(hgr.getId());
+                            measure.setConsumedInRound(null);
+                            measure.store();
+                            measureCost += mt.getPrice();
+                            measureSat += mt.getSatisfactionDelta();
+                        }
+                    }
+                }
+
+                int satPoints = satPointsStr.length() == 0 ? 0 : Integer.parseInt(satPointsStr);
+                int satPointCost = satPoints * wft.getSatisfactionCostPerPoint();
+
+                hgr.setHouseSatisfaction(hgr.getHouseSatisfaction() + measureSat);
+                prr.setSatisfactionHouseMeasures(measureSat);
+                prr.setCostMeasuresBought(measureCost);
+                hgr.store();
+
+                prr.setSatisfactionBought(satPoints);
+                prr.setCostSatisfactionBought(satPointCost);
+                prr.setPersonalSatisfaction(prr.getPersonalSatisfaction() + satPoints);
+                prr.setSpendableIncome(prr.getSpendableIncome() - measureCost - satPointCost);
+                prr.setPlayerState(PlayerState.ANSWER_SURVEY.toString());
+                prr.store();
+
+                response.sendRedirect("/housinggame-player/answer-survey");
+                return;
+            }
+
+            // if the player did not click 'view house' and enters the read-news-done servlet, something is wrong
+            data.errorRedirect(response,
+                    "Player app called view-improvements-done servlet, but NextScreen button is " + nextScreen);
+        }
+        catch (Exception e)
+        {
+            data.errorRedirect(response,
+                    "Player app called view-improvements-done servlet, but error occurred: " + e.getMessage());
+        }
     }
 
     @Override
