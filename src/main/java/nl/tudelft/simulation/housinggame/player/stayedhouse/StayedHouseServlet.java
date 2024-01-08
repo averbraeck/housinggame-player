bb3766162d05d1aad09f7ea80e93b8ae112eae5c
Alexander Verbraeck <a.verbraeck@gmail.com>
2024-01-08 03:33:11 +0100

    Issue #23. Make stayed-house servlet

---
+++ src/main/java/nl/tudelft/simulation/housinggame/player/stayedhouse/StayedHouseServlet.java
@@ -0,0 +1,51 @@
+package nl.tudelft.simulation.housinggame.player.stayedhouse;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import nl.tudelft.simulation.housinggame.player.PlayerData;
+import nl.tudelft.simulation.housinggame.player.house.HouseAccordion;
+import nl.tudelft.simulation.housinggame.player.readbudget.BudgetAccordion;
+import nl.tudelft.simulation.housinggame.player.readnews.NewsAccordion;
+
+@WebServlet("/stayed-house")
+public class StayedHouseServlet extends HttpServlet
+{
+
+    /** */
+    private static final long serialVersionUID = 1L;
+
+    @Override
+    protected void doPost(final HttpServletRequest request, final HttpServletResponse response)
+            throws ServletException, IOException
+    {
+        HttpSession session = request.getSession();
+
+        PlayerData data = (PlayerData) session.getAttribute("playerData");
+        if (data == null)
+        {
+            response.sendRedirect("/housinggame-player/login");
+            return;
+        }
+
+        data.getContentHtml().clear();
+        BudgetAccordion.makeBudgetAccordion(data);
+        NewsAccordion.makeNewsAccordion(data);
+        HouseAccordion.makeHouseConfirmationAccordion(data);
+
+        response.sendRedirect("jsp/player/stayed-house.jsp");
+    }
+
+    @Override
+    protected void doGet(final HttpServletRequest req, final HttpServletResponse resp) throws ServletException, IOException
+    {
+        doPost(req, resp);
+    }
+
+}
