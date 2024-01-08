b240f22bc635af82b8bea4923753704b4797e730
Alexander Verbraeck <a.verbraeck@gmail.com>
2024-01-08 03:14:23 +0100

    Issue #20. First version of stay-house-wait screen

---
+++ src/main/java/nl/tudelft/simulation/housinggame/player/stayhousewait/StayHouseWaitServlet.java
@@ -0,0 +1,85 @@
+package nl.tudelft.simulation.housinggame.player.stayhousewait;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import nl.tudelft.simulation.housinggame.data.Tables;
+import nl.tudelft.simulation.housinggame.data.tables.records.HousegroupRecord;
+import nl.tudelft.simulation.housinggame.data.tables.records.HousetransactionRecord;
+import nl.tudelft.simulation.housinggame.data.tables.records.PlayerroundRecord;
+import nl.tudelft.simulation.housinggame.player.PlayerData;
+import nl.tudelft.simulation.housinggame.player.SqlUtils;
+import nl.tudelft.simulation.housinggame.player.readbudget.BudgetAccordion;
+import nl.tudelft.simulation.housinggame.player.readnews.NewsAccordion;
+
+@WebServlet("/stay-house-wait")
+public class StayHouseWaitServlet extends HttpServlet
+{
+
+    /** */
+    private static final long serialVersionUID = 1L;
+
+    @Override
+    protected void doPost(final HttpServletRequest request, final HttpServletResponse response)
+            throws ServletException, IOException
+    {
+        HttpSession session = request.getSession();
+
+        PlayerData data = (PlayerData) session.getAttribute("playerData");
+        if (data == null)
+        {
+            response.sendRedirect("/housinggame-player/login");
+            return;
+        }
+
+        data.getContentHtml().clear();
+        BudgetAccordion.makeBudgetAccordion(data);
+        NewsAccordion.makeNewsAccordion(data);
+        makeHouseWaitConfirmationAccordion(data);
+
+        response.sendRedirect("jsp/player/stay-house-wait.jsp");
+    }
+
+    @Override
+    protected void doGet(final HttpServletRequest req, final HttpServletResponse resp) throws ServletException, IOException
+    {
+        doPost(req, resp);
+    }
+
+    private static void makeHouseWaitConfirmationAccordion(final PlayerData data)
+    {
+        PlayerroundRecord playerRound = data.getPlayerRound();
+        StringBuilder s = new StringBuilder();
+        s.append("            <div>\n");
+        if (playerRound.getActiveTransactionId() == null)
+        {
+            s.append("<p class=\"hg-box-red\">\n");
+            s.append("You have not been allocated a house in this round or an earlier round.\n");
+            s.append("Without a house, you cannot fully participate in the game, since you cannot \n");
+            s.append("experience the effect of flooding, nor buy measures to improve your house.\n");
+            s.append("Get a house allocation in the next round!\n");
+            s.append("</div>\n");
+        }
+        else
+        {
+            HousetransactionRecord transaction =
+                    SqlUtils.readRecordFromId(data, Tables.HOUSETRANSACTION, playerRound.getActiveTransactionId());
+            HousegroupRecord houseGroup = SqlUtils.readRecordFromId(data, Tables.HOUSEGROUP, transaction.getHousegroupId());
+            s.append("<p>You have opted to stay in house " + houseGroup.getCode() + "<br/>\n");
+            s.append("<p>\n");
+            s.append("If the facilitator approves your staying request, you can continue.\n");
+            s.append("Otherwise, you will go back to the view house screen to select another option.\n");
+            s.append("</p>\n");
+        }
+        s.append("            </div>\n");
+        data.getContentHtml().put("panel/house", s.toString());
+        data.getContentHtml().put("panel/house/title", "3. Your house choice");
+    }
+
+}
---
+++ src/main/webapp/jsp/player/stay-house-wait.jsp
@@ -0,0 +1,83 @@
+<%@page import="nl.tudelft.simulation.housinggame.player.PlayerData"%>
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+  pageEncoding="ISO-8859-1"%>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <jsp:include page="head.jsp"></jsp:include>
+  <title>Housing Game: Select House</title>
+  
+<style>
+.hg-house-row {
+  display: flex;
+  flex-direction: row;
+  padding: 2px;
+  margin: 2px;  
+}
+
+.hg-house-icon {
+  padding: 2px;
+  margin: 2px;
+  display-vertical: center;
+  opacity: 50%;
+}
+</style>
+
+</head>
+
+<body style="background-color: white;" onLoad = initPage()>
+
+  <div class="form-container">
+    
+    <jsp:include page="header.jsp"></jsp:include>
+    
+    <div class="hg-title">Wait for the facilitator<br/>to check your staying request</div>
+    
+    <div class="panel-group pmd-accordion" id="hg-accordion" role="tablist" aria-multiselectable="true" > 
+      <jsp:include page="accordion1.jsp"></jsp:include>
+      <jsp:include page="accordion2.jsp"></jsp:include>
+      <jsp:include page="accordion3.jsp">
+        <jsp:param name="open" value="in" />
+      </jsp:include>
+    </div>
+   
+    <div style="display:flex; flex-direction: row; justify-content: space-around;"> 
+      <form action="/housinggame-player/stay-house-accept-done" method="post">
+        <div class="hg-button">
+          <input type="hidden" name="nextScreen" value="accept-stay" />
+          <input type="submit" value="APPROVED: STAY" class="btn btn-primary" id="hg-stay" disabled />
+        </div>
+      </form>
+      <form action="/housinggame-player/stay-house-reject-done" method="post">
+        <div class="hg-button">
+          <input type="hidden" name="nextScreen" value="reject-stay" />
+          <input type="submit" value="REJECTED: GO BACK" class="btn btn-primary" id="hg-goback" disabled />
+        </div>
+      </form>
+    </div>
+    
+    <br/>&nbsp;<br/>
+    
+  </div>
+  
+  <script>
+    $(document).ready(function() {
+      check();
+    });
+    function check() {
+        $.post("/housinggame-player/check-buy-status", {jsp: 'stay-house-wait'},
+          function(data, status) {
+            if (data == "APPROVED") {
+              $("#hg-stay").removeAttr("disabled");
+            } else if (data == "REJECTED") {
+              $("#hg-goback").removeAttr("disabled");
+            } else {
+              setTimeout(check, 5000);
+            }
+          });
+    }
+  </script>
+  
+</body>
+</html>
