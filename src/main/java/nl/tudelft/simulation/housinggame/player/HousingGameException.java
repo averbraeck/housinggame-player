805cc0a07e5b783e9a37dc67f27dab5d252ab09e
Alexander Verbraeck <a.verbraeck@gmail.com>
2024-02-23 13:54:28 +0100

    Copyright updated to 2024

--- src/main/java/nl/tudelft/simulation/housinggame/player/HousingGameException.java
+++ src/main/java/nl/tudelft/simulation/housinggame/player/HousingGameException.java
@@ -3,7 +3,7 @@ package nl.tudelft.simulation.housinggame.player;
 /**
  * HousingGameException.java.
  * <p>
- * Copyright (c) 2020-2020 Delft University of Technology, PO Box 5, 2600 AA, Delft, the Netherlands. All rights reserved. <br>
+ * Copyright (c) 2020-2024 Delft University of Technology, PO Box 5, 2600 AA, Delft, the Netherlands. All rights reserved. <br>
  * BSD-style license. 
  * </p>
  * @author <a href="https://www.tudelft.nl/averbraeck">Alexander Verbraeck</a>
--- src/main/java/nl/tudelft/simulation/housinggame/player/readbudget/BudgetAccordion.java
+++ src/main/java/nl/tudelft/simulation/housinggame/player/readbudget/BudgetAccordion.java
@@ -10,7 +10,7 @@ import nl.tudelft.simulation.housinggame.player.SqlUtils;
  * BudgetAccordion puts the html-code for the budget in panel/budget, and progressively increases the amount of information
  * during the round.
  * <p>
- * Copyright (c) 2020-2020 Delft University of Technology, PO Box 5, 2600 AA, Delft, the Netherlands. All rights reserved. <br>
+ * Copyright (c) 2020-2024 Delft University of Technology, PO Box 5, 2600 AA, Delft, the Netherlands. All rights reserved. <br>
  * BSD-style license. See <a href="https://opentrafficsim.org/docs/current/license.html">OpenTrafficSim License</a>.
  * </p>
  * @author <a href="https://www.tudelft.nl/averbraeck">Alexander Verbraeck</a>
