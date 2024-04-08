404202cc1681bff50992f5074f33090931c9a7cb
Alexander Verbraeck <a.verbraeck@gmail.com>
2024-04-08 03:58:12 +0200

    Issue #60. Repair damage screen

--- src/main/java/nl/tudelft/simulation/housinggame/player/viewdamage/DamageAccordion.java
+++ src/main/java/nl/tudelft/simulation/housinggame/player/viewdamage/DamageAccordion.java
@@ -71,17 +71,6 @@ public class DamageAccordion
 
             // @formatter:off
 
-            // Rain flood info
-
-            s.append("            <div class=\"hg-header1\">Rain flood info</div>\n");
-            s.append("            <div class=\"hg-box-grey\">\n");
-            s.append("              Community rain protection: " + pCommProt + "<br/>\n");
-            s.append("              House rain protection: " + pHouseProt + "<br/>\n");
-            s.append("              Rain flood intensity: " + pDice + "<br/>\n");
-            s.append("              Community damage due to rain: " + Math.max(0, pDice - pCommProt) + "<br/>\n");
-            s.append("              House damage due to rain: " + Math.max(0, pDice - pHouseProt) + "<br/>\n");
-            s.append("            </div>\n");
-
             // River flood info
 
             s.append("            <div class=\"hg-header1\">River flood info</div>\n");
@@ -93,12 +82,23 @@ public class DamageAccordion
             s.append("              House damage due to river: " + Math.max(0, fDice - fHouseProt) + "<br/>\n");
             s.append("            </div>\n");
 
+            // Rain flood info
+
+            s.append("            <div class=\"hg-header1\">Rain flood info</div>\n");
+            s.append("            <div class=\"hg-box-grey\">\n");
+            s.append("              Community rain protection: " + pCommProt + "<br/>\n");
+            s.append("              House rain protection: " + pHouseProt + "<br/>\n");
+            s.append("              Rain flood intensity: " + pDice + "<br/>\n");
+            s.append("              Community damage due to rain: " + Math.max(0, pDice - pCommProt) + "<br/>\n");
+            s.append("              House damage due to rain: " + Math.max(0, pDice - pHouseProt) + "<br/>\n");
+            s.append("            </div>\n");
+
             // Costs and Penalties
 
             s.append("            <div class=\"hg-header1\">Cumulative costs and penalties</div>\n");
             s.append("            <div class=\"hg-box-grey\">\n");
-            s.append("              Costs for rain damage repairs: " + prr.getCostPluvialDamage() + "<br/>\n");
             s.append("              Costs for river damage repairs: " + prr.getCostFluvialDamage() + "<br/>\n");
+            s.append("              Costs for rain damage repairs: " + prr.getCostPluvialDamage() + "<br/>\n");
             s.append("              Satisfaction penalty: " +
                     (prr.getSatisfactionPluvialPenalty() + prr.getSatisfactionFluvialPenalty()) + "<br/>\n");
             s.append("            </div>\n");
@@ -111,19 +111,19 @@ public class DamageAccordion
             s.append("                <tr>\n");
             s.append("                  <th>Round</th>\n");
             s.append("                  <th>Owned</th>\n");
-            s.append("                  <th colspan=\"2\">Flood level</th>\n");
-            s.append("                  <th colspan=\"2\">Comm. prot.</th>\n");
-            s.append("                  <th colspan=\"2\">House prot.</th>\n");
+            s.append("                  <th colspan=\"2\">Flood<br/>level</th>\n");
+            s.append("                  <th colspan=\"2\">Community<br/>protection</th>\n");
+            s.append("                  <th colspan=\"2\">House<br/>protection</th>\n");
             s.append("                </tr>\n");
             s.append("                <tr>\n");
             s.append("                  <th>Nr.</th>\n");
             s.append("                  <th>House</th>\n");
-            s.append("                  <th>Rain</th>\n");
             s.append("                  <th>River</th>\n");
             s.append("                  <th>Rain</th>\n");
             s.append("                  <th>River</th>\n");
             s.append("                  <th>Rain</th>\n");
             s.append("                  <th>River</th>\n");
+            s.append("                  <th>Rain</th>\n");
             s.append("                </tr>\n");
             s.append("              </thead>\n");
             s.append("              <tbody>\n");
@@ -138,24 +138,24 @@ public class DamageAccordion
                 if (p.getFinalHousegroupId() == null)
                 {
                     s.append("<td>--</td>\n");
-                    s.append("<td>" + g.getPluvialFloodIntensity() + "</td>\n");
                     s.append("<td>" + g.getFluvialFloodIntensity() + "</td>\n");
+                    s.append("<td>" + g.getPluvialFloodIntensity() + "</td>\n");
                     s.append("<td>--</td><td>--</td><td>--</td><td>--</td>\n");
                 }
                 else
                 {
                     var h = SqlUtils.readRecordFromId(data, Tables.HOUSEGROUP, p.getFinalHousegroupId());
                     s.append("<td>" + h.getCode() + "</td>\n");
-                    s.append("<td>" + g.getPluvialFloodIntensity() + "</td>\n");
                     s.append("<td>" + g.getFluvialFloodIntensity() + "</td>\n");
-                    int pcp = p.getPluvialBaseProtection() + p.getPluvialCommunityDelta();
+                    s.append("<td>" + g.getPluvialFloodIntensity() + "</td>\n");
                     int fcp = p.getFluvialBaseProtection() + p.getFluvialCommunityDelta();
-                    int php = pcp + p.getPluvialHouseDelta();
+                    int pcp = p.getPluvialBaseProtection() + p.getPluvialCommunityDelta();
                     int fhp = fcp + p.getFluvialHouseDelta();
-                    s.append("<td>" + pcp + "</td>\n");
+                    int php = pcp + p.getPluvialHouseDelta();
                     s.append("<td>" + fcp + "</td>\n");
-                    s.append("<td>" + php + "</td>\n");
+                    s.append("<td>" + pcp + "</td>\n");
                     s.append("<td>" + fhp + "</td>\n");
+                    s.append("<td>" + php + "</td>\n");
                 }
                 s.append("                </tr>\n");
             }
@@ -164,7 +164,7 @@ public class DamageAccordion
 
             // Damage history
 
-            s.append("            <div class=\"hg-header1\">Cumulative damage history</div>\n");
+            s.append("            <div class=\"hg-header1\">Damage history</div>\n");
             s.append("            <table class=\"hg-table\">\n");
             s.append("              <thead>\n");
             s.append("                <tr>\n");
@@ -176,10 +176,10 @@ public class DamageAccordion
             s.append("                <tr>\n");
             s.append("                  <th>Nr.</th>\n");
             s.append("                  <th>House</th>\n");
-            s.append("                  <th>Rain</th>\n");
             s.append("                  <th>River</th>\n");
             s.append("                  <th>Rain</th>\n");
             s.append("                  <th>River</th>\n");
+            s.append("                  <th>Rain</th>\n");
             s.append("                </tr>\n");
             s.append("              </thead>\n");
             s.append("              <tbody>\n");
@@ -200,10 +200,10 @@ public class DamageAccordion
                 {
                     var h = SqlUtils.readRecordFromId(data, Tables.HOUSEGROUP, p.getFinalHousegroupId());
                     s.append("<td>" + h.getCode() + "</td>\n");
-                    s.append("<td>" + p.getSatisfactionPluvialPenalty() + "</td>\n");
                     s.append("<td>" + p.getSatisfactionFluvialPenalty() + "</td>\n");
-                    s.append("<td>" + data.k(p.getCostPluvialDamage()) + "</td>\n");
+                    s.append("<td>" + p.getSatisfactionPluvialPenalty() + "</td>\n");
                     s.append("<td>" + data.k(p.getCostFluvialDamage()) + "</td>\n");
+                    s.append("<td>" + data.k(p.getCostPluvialDamage()) + "</td>\n");
                 }
                 s.append("                </tr>\n");
             }
